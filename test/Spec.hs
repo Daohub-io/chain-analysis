07a573dfeabd485aa132d06ce2cff9bf616b37fb
Jake O'Shannessy <joshannessy@gmail.com>
2018-03-20 14:03:17 +0300

    add cabal file

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,9 +2,12 @@ module Main where
 
 import Data.Attoparsec.ByteString
 import Data.ByteString (pack)
+import qualified Data.ByteString.Char8 as C8 (pack)
+import Data.ByteString.Base16
 import Data.Maybe
 import Data.Monoid (mempty)
 import System.Environment
+
 
 import Test.Framework (defaultMain, defaultMainWithOpts, testGroup)
 import Test.Framework.Options (TestOptions, TestOptions'(..))
@@ -60,9 +63,15 @@ parseSTOPTest = TestLabel "Parse STOP OpCode" $ TestCase $ do
     let res = parseOnly (parseSTOP <* endOfInput) (pack [0x00])
     case res of
         Right STOP -> pure ()
-        _ -> error "FSD"
+        _ -> assertFailure $ "STOP should be parsed, but was not"
+-- |Parse something that is not the STOP OpCode
 parseSTOPTestNot = TestLabel "Parse STOP OpCode Not" $ TestCase $ do
-    let res = parseOnly (parseSTOP <* endOfInput) (pack [0x01])
-    case res of
+    case parseOnly (parseSTOP <* endOfInput) (pack [0x01]) of
         Left _ -> pure ()
-        Right _ -> error "Fds"
+        Right oc -> assertFailure $ "No opcode should be parsed, but " ++ show oc ++ " was parsed"
+
+testExampleContract = TestLabel "Parse Example Contract" $ TestCase $ do
+    let (bs,_) = decode $ C8.pack "6060604052341561000f57600080fd5b60ba8061001d6000396000f300606060405260043610603f576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff168063771602f7146044575b600080fd5b3415604e57600080fd5b606b60048080359060200190919080359060200190919050506081565b6040518082815260200191505060405180910390f35b60008183019050929150505600a165627a7a72305820208e94342b2b01f28a6a3e363d85bb930b900adbc78c5ac5c926c3c316c993840029"
+    case parseOnly (parseOpCodes <* endOfInput) (pack [0x01]) of
+        Left _ -> assertFailure $ "Opcodes should be parsed in fulle"
+        Right ocs -> pure ()
