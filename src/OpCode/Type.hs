9b9e78e276d43b7b39ab26c7af580600de992db1
Jake O'Shannessy <joshannessy@gmail.com>
2018-03-28 18:45:40 +0300

    Added custom Show instance for OpCode

--- src/OpCode/Type.hs
+++ src/OpCode/Type.hs
@@ -4,7 +4,7 @@ import Prelude hiding (LT, EQ, GT)
 
 import Control.Applicative
 import Data.ByteString
-import Data.ByteString.Base16 (encode)
+import Data.ByteString.Base16 as B16 (encode)
 import qualified Data.ByteString.Char8 as C8
 import Data.Attoparsec.ByteString as A
 -- import Data.Attoparsec.Binary
@@ -16,6 +16,8 @@ import Data.Word
 import Control.Monad
 
 import Test.QuickCheck
+
+import OpCode.Utils
 
 data OpCode
     = STOP
@@ -153,7 +155,144 @@ data OpCode
     | REVERT
     | INVALID
     | SELFDESTRUCT
-    deriving (Eq, Show)
+    deriving (Eq)
+
+instance Show OpCode where
+    show STOP = "STOP"
+    show ADD = "ADD"
+    show MUL = "MUL"
+    show SUB = "SUB"
+    show DIV = "DIV"
+    show SDIV = "SDIV"
+    show MOD = "MOD"
+    show SMOD = "SMOD"
+    show ADDMOD = "ADDMOD"
+    show MULMOD = "MULMOD"
+    show EXP = "EXP"
+    show SIGNEXTEND = "SIGNEXTEND"
+    show LT = "LT"
+    show GT = "GT"
+    show SLT = "SLT"
+    show SGT = "SGT"
+    show EQ = "EQ"
+    show ISZERO = "ISZERO"
+    show AND = "AND"
+    show OR = "OR"
+    show XOR = "XOR"
+    show NOT = "NOT"
+    show BYTE = "BYTE"
+    show SHA3 = "SHA3"
+    show ADDRESS = "ADDRESS"
+    show BALANCE = "BALANCE"
+    show ORIGIN = "ORIGIN"
+    show CALLER = "CALLER"
+    show CALLVALUE = "CALLVALUE"
+    show CALLDATALOAD = "CALLDATALOAD"
+    show CALLDATASIZE = "CALLDATASIZE"
+    show CALLDATACOPY = "CALLDATACOPY"
+    show CODESIZE = "CODESIZE"
+    show CODECOPY = "CODECOPY"
+    show GASPRICE = "GASPRICE"
+    show EXTCODESIZE = "EXTCODESIZE"
+    show EXTCODECOPY = "EXTCODECOPY"
+    show RETURNDATASIZE = "RETURNDATASIZE"
+    show RETURNDATACOPY = "RETURNDATACOPY"
+    show BLOCKHASH = "BLOCKHASH"
+    show COINBASE = "COINBASE"
+    show TIMESTAMP = "TIMESTAMP"
+    show NUMBER = "NUMBER"
+    show DIFFICULTY = "DIFFICULTY"
+    show GASLIMIT = "GASLIMIT"
+    show POP = "POP"
+    show MLOAD = "MLOAD"
+    show MSTORE = "MSTORE"
+    show MSTORE8 = "MSTORE8"
+    show SLOAD = "SLOAD"
+    show SSTORE = "SSTORE"
+    show JUMP = "JUMP"
+    show JUMPI = "JUMPI"
+    show PC = "PC"
+    show MSIZE = "MSIZE"
+    show GAS = "GAS"
+    show JUMPDEST = "JUMPDEST"
+    show (PUSH1 bytes) = "PUSH1 " ++ show (B16.encode bytes)
+    show (PUSH2 bytes) = "PUSH2 " ++ show (B16.encode bytes)
+    show (PUSH3 bytes) = "PUSH3 " ++ show (B16.encode bytes)
+    show (PUSH4 bytes) = "PUSH4 " ++ show (B16.encode bytes)
+    show (PUSH5 bytes) = "PUSH5 " ++ show (B16.encode bytes)
+    show (PUSH6 bytes) = "PUSH6 " ++ show (B16.encode bytes)
+    show (PUSH7 bytes) = "PUSH7 " ++ show (B16.encode bytes)
+    show (PUSH8 bytes) = "PUSH8 " ++ show (B16.encode bytes)
+    show (PUSH9 bytes) = "PUSH9 " ++ show (B16.encode bytes)
+    show (PUSH10 bytes) = "PUSH10 " ++ show (B16.encode bytes)
+    show (PUSH11 bytes) = "PUSH11 " ++ show (B16.encode bytes)
+    show (PUSH12 bytes) = "PUSH12 " ++ show (B16.encode bytes)
+    show (PUSH13 bytes) = "PUSH13 " ++ show (B16.encode bytes)
+    show (PUSH14 bytes) = "PUSH14 " ++ show (B16.encode bytes)
+    show (PUSH15 bytes) = "PUSH15 " ++ show (B16.encode bytes)
+    show (PUSH16 bytes) = "PUSH16 " ++ show (B16.encode bytes)
+    show (PUSH17 bytes) = "PUSH17 " ++ show (B16.encode bytes)
+    show (PUSH18 bytes) = "PUSH18 " ++ show (B16.encode bytes)
+    show (PUSH19 bytes) = "PUSH19 " ++ show (B16.encode bytes)
+    show (PUSH20 bytes) = "PUSH20 " ++ show (B16.encode bytes)
+    show (PUSH21 bytes) = "PUSH21 " ++ show (B16.encode bytes)
+    show (PUSH22 bytes) = "PUSH22 " ++ show (B16.encode bytes)
+    show (PUSH23 bytes) = "PUSH23 " ++ show (B16.encode bytes)
+    show (PUSH24 bytes) = "PUSH24 " ++ show (B16.encode bytes)
+    show (PUSH25 bytes) = "PUSH25 " ++ show (B16.encode bytes)
+    show (PUSH26 bytes) = "PUSH26 " ++ show (B16.encode bytes)
+    show (PUSH27 bytes) = "PUSH27 " ++ show (B16.encode bytes)
+    show (PUSH28 bytes) = "PUSH28 " ++ show (B16.encode bytes)
+    show (PUSH29 bytes) = "PUSH29 " ++ show (B16.encode bytes)
+    show (PUSH30 bytes) = "PUSH30 " ++ show (B16.encode bytes)
+    show (PUSH31 bytes) = "PUSH31 " ++ show (B16.encode bytes)
+    show (PUSH32 bytes) = "PUSH32 " ++ show (B16.encode bytes)
+    show DUP1 = "DUP1"
+    show DUP2 = "DUP2"
+    show DUP3 = "DUP3"
+    show DUP4 = "DUP4"
+    show DUP5 = "DUP5"
+    show DUP6 = "DUP6"
+    show DUP7 = "DUP7"
+    show DUP8 = "DUP8"
+    show DUP9 = "DUP9"
+    show DUP10 = "DUP10"
+    show DUP11 = "DUP11"
+    show DUP12 = "DUP12"
+    show DUP13 = "DUP13"
+    show DUP14 = "DUP14"
+    show DUP15 = "DUP15"
+    show DUP16 = "DUP16"
+    show SWAP1 = "SWAP1"
+    show SWAP2 = "SWAP2"
+    show SWAP3 = "SWAP3"
+    show SWAP4 = "SWAP4"
+    show SWAP5 = "SWAP5"
+    show SWAP6 = "SWAP6"
+    show SWAP7 = "SWAP7"
+    show SWAP8 = "SWAP8"
+    show SWAP9 = "SWAP9"
+    show SWAP10 = "SWAP10"
+    show SWAP11 = "SWAP11"
+    show SWAP12 = "SWAP12"
+    show SWAP13 = "SWAP13"
+    show SWAP14 = "SWAP14"
+    show SWAP15 = "SWAP15"
+    show SWAP16 = "SWAP16"
+    show LOG0 = "LOG0"
+    show LOG1 = "LOG1"
+    show LOG2 = "LOG2"
+    show LOG3 = "LOG3"
+    show LOG4 = "LOG4"
+    show CREATE = "CREATE"
+    show CALL = "CALL"
+    show CALLCODE = "CALLCODE"
+    show RETURN = "RETURN"
+    show DELEGATECALL = "DELEGATECALL"
+    show STATICCALL = "STATICCALL"
+    show REVERT = "REVERT"
+    show INVALID = "INVALID"
+    show SELFDESTRUCT = "SELFDESTRUCT"
 
 nBytes :: OpCode -> Integer
 nBytes (PUSH1 _) = 1
